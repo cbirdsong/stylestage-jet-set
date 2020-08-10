7649168c6973f62e978679caf2b47295eb5e9de2
cbirdsong <cory@birdsong.dev>
2020-08-10 16:23:01 -0500

    Add cache busting

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,20 @@
+const { DateTime } = require("luxon");
+
 module.exports = function (eleventyConfig) {
 	// Don't process folders with static assets e.g. images
 	eleventyConfig.addPassthroughCopy("fonts/");
 	eleventyConfig.addPassthroughCopy("scss/");
-	eleventyConfig.addPassthroughCopy("style.css");
-	eleventyConfig.addPassthroughCopy("style.css.map");
-
 	eleventyConfig.addWatchTarget("./scss/");
 	eleventyConfig.addWatchTarget("style.css");
 
+	// Date formatting (machine readable)
+	eleventyConfig.addFilter("utcDate", dateObj => {
+		return DateTime.fromJSDate(dateObj).toFormat("yyyyMMddhhmmss");
+	});
+
+
 	return {
 		templateFormats: ["html"],
+		htmlTemplateEngine: ["njk"],
 	};
 };
