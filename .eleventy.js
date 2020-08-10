bb6bb7b5ce92fc55435cf2bedda5a55d67f277c3
cbirdsong <cory@birdsong.dev>
2020-08-10 16:30:53 -0500

    Add passthrough copy

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,9 @@ module.exports = function (eleventyConfig) {
 	// Don't process folders with static assets e.g. images
 	eleventyConfig.addPassthroughCopy("fonts/");
 	eleventyConfig.addPassthroughCopy("scss/");
+	eleventyConfig.addPassthroughCopy("style.css");
+	eleventyConfig.addPassthroughCopy("style.css.map");
+
 	eleventyConfig.addWatchTarget("./scss/");
 	eleventyConfig.addWatchTarget("style.css");
 
